b36bb77e751c8b1e4d192dfb851fec4d78fd79fb
Nitin <nsinghboss02@gmail.com>
2022-01-05 10:20:50 +0530

    Initial commit

---
+++ src/com/codechef/Main.java
@@ -0,0 +1,17 @@
+package com.codechef;
+import java.util.LinkedList;
+public class Main {
+
+    public static void main(String[] args) {
+	// linkedlist implementation with its internal working :
+        /*
+        it uses a doubly linked list internally to store and organize the data:
+         */
+        LinkedList <Integer> list = new LinkedList<>();
+        list.add(67);
+        list.add(87);
+        list.addFirst(65);
+        list.set(0,86);
+        System.out.println(list);
+    }
+}
